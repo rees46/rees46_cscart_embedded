346cfab10b5dd49b99a6fdb4aaa70c5cfc6d524f
Michael Kechinov <sam@mkechinov.ru>
2015-08-30 13:38:52 +0300

    Closes #8

--- app/addons/rees46/func.php
+++ app/addons/rees46/func.php
@@ -8,8 +8,8 @@ use Tygh\Registry;
 
 function fn_rees46_generate_info()
 {
-    if (Config::getShopID() == '') {
-        return '
+	if (Config::getShopID() == '') {
+		return '
         <p>
           <h3>' . __('rees46_text_settings_title') . ':</h3>
           <ol>
@@ -17,43 +17,43 @@ function fn_rees46_generate_info()
           </ol>
         </p>
         ';
-    } else {
-        $res = __('rees46_text_settings_info');
+	} else {
+		$res = __('rees46_text_settings_info');
 
-        $res = $res . '<p><a href="/admin.php?dispatch=rees46.export_orders" class="btn btn-primary">' . __('rees46_export_order') . '</a> (' . __('rees46_export_order_hint') . ')</p>';
+		$res = $res . '<p><a href="' . fn_url('rees46.export_orders') . '" class="btn btn-primary">' . __('rees46_export_order') . '</a> (' . __('rees46_export_order_hint') . ')</p>';
 
-        return $res;
-    }
+		return $res;
+	}
 }
 
 function fn_rees46_add_to_cart($cart, $product_id, $_id)
 {
-    Events::CookieEvent('cart', array('item_id' => $product_id));
+	Events::CookieEvent('cart', array('item_id' => $product_id));
 }
 
 function fn_rees46_delete_cart_product($cart, $cart_id, $full_erase)
 {
-    if (!empty($cart_id) && !empty($cart['products'][$cart_id])) {
-        if (!empty($cart['products'][$cart_id]['product_id'])) {
-            $product_id = $cart['products'][$cart_id]['product_id'];
+	if (!empty($cart_id) && !empty($cart['products'][$cart_id])) {
+		if (!empty($cart['products'][$cart_id]['product_id'])) {
+			$product_id = $cart['products'][$cart_id]['product_id'];
 
-            Events::CookieEvent('remove_from_cart', array('item_id' => $product_id));
-        }
-    }
+			Events::CookieEvent('remove_from_cart', array('item_id' => $product_id));
+		}
+	}
 }
 
 function fn_rees46_place_order($order_id, $action, $order_status, $cart, $auth)
 {
-    $data = array(
-        'items'     => array(),
-        'order_id'  => $order_id,
-    );
+	$data = array(
+		'items'     => array(),
+		'order_id'  => $order_id,
+	);
 
-    foreach ($cart['products'] as $product) {
-        array_push($data['items'], array('item_id' => $product['product_id'], 'amount'  => $product['amount']));
-    }
+	foreach ($cart['products'] as $product) {
+		array_push($data['items'], array('item_id' => $product['product_id'], 'amount'  => $product['amount']));
+	}
 
-    Events::CookieEvent('order', $data);
+	Events::CookieEvent('order', $data);
 }
 
 //Хук к списку товаров
