be7587e61255976d9bd85def3f87be49fc4c25ad
Andrey Veprikov <av@rees46.com>
2015-11-25 17:42:15 +0300

    Checking required PHP version

--- app/addons/rees46/controllers/frontend/init.post.php
+++ app/addons/rees46/controllers/frontend/init.post.php
@@ -2,4 +2,9 @@
 use Tygh\Rees46\Init;
 use Tygh\Registry;
 
-Registry::get('view')->assign('rees46', Init::getGlobal());
+$php_value = phpversion();
+if (version_compare($php_value, '5.4.0') == -1) {
+	fn_set_notification('E', __('rees46_php_version'), '', 'I');
+} else {
+	Registry::get('view')->assign('rees46', Init::getGlobal());
+}
